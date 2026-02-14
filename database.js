2150f347437fad99a2eb06da207694662843376a
Issam Kebdani <kebdaniissam780@gmail.com>
2026-02-14 21:27:25 +0100

    link railway database

--- database.js
+++ database.js
@@ -2,10 +2,5 @@ import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
-export const pool = mysql.createPool({ 
-    host: process.env.MYSQL_HOST,  
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_NAME,
-});
+const urlDB = "mysql://root:RrgKstUuVqlJfRyUwVqeKIsrugwYLNug@mysql.railway.internal:3306/railway"
+export const pool = mysql.createPool( urlDB );    
